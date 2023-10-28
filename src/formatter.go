07027c994e74840be89e5a04836cac609dcdcb10
Andrey Ustinov <austinov@gmail.com>
2023-10-27 17:25:38 -0700

    Better

--- src/formatter.go
+++ src/formatter.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	// "github.com/fatih/color"
 	"github.com/gookit/color"
+	"fmt"
 )
 
 var (
@@ -40,6 +41,7 @@ func OptimizeFile(fpath string) (string, string) {
 
 func FormatFile(fname string) string {
 	d, f := OptimizeFile(fname)
+	fmt.Println("FormatFile", d, f)
 	return f + " " + d
 }
 
@@ -72,37 +74,112 @@ func stripColor(text string) string {
 	return toPlainText(result)
 }
 
-func FormatGrep(line string) string {
+func formatFname(fname string) string {
 	fnameStyle := color.HEX("9da").Sprint
+	dimStyle := color.HEX("777").Sprint
+
+	name := path.Base(fname)
+	dir := path.Dir(fname) // ShortenDir(path.Dir(fname))
+	return fnameStyle(name) + " " + dimStyle(dir)
+}
+
+func formatPrefix(filepath string, line string, column string) string {
+	fnameStyle := color.HEX("9da").Sprint
+	dirStyle := color.HEX("888").Sprint
+	sepStyle := color.HEX("666").Sprint
 	lineStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
 	columnStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
-	dimStyle := color.HEX("777").Sprint
+
+	fname := path.Base(filepath)
+	dir := path.Dir(filepath)
+
+	return fnameStyle(fname) + sepStyle(":") + lineStyle(line) + sepStyle(":") + columnStyle(column) + " " + dirStyle(dir) + sepStyle(":")
+}
+
+func splitLine(line string) (string, string, string, string) {
 	parts := strings.SplitN(line, ":", 4)
-	result := ""
-	if len(parts) > 0 {
-		result += fnameStyle(stripColor(parts[0]))
-		result += dimStyle(":")
-	}
-	if len(parts) > 1 {
-		result += lineStyle(stripColor(parts[1]))
-		result += dimStyle(":")
-	}
-	if len(parts) > 2 {
-		result += columnStyle(stripColor(parts[2]))
-		result += dimStyle(":")
-	}
 	if len(parts) > 3 {
-		result += parts[3]
-		// colored, err := ansi_parser.Parse(parts[3], ansi_parser.WithIgnoreInvalidCodes(), ansi_parser.ParseOption{NonColorCodes: ansi_parser.Remove})
-		// if err != nil {
-		// 	result += "ERR:" + parts[3]
-		// }
-		// for _, t := range colored {
-		// 	result += formatStyle(t)
-		// }
-		// result += parts[3]
+		// vimgrep - file:line:column:text
+		lnum := stripColor(parts[1])
+		_, err := strconv.Atoi(lnum)
+		if err == nil {
+			cnum := stripColor(parts[2])
+			_, err := strconv.Atoi(cnum)
+			if err == nil {
+				return stripColor(parts[0]), stripColor(parts[1]), stripColor(parts[2]), parts[3]
+			}
+			return stripColor(parts[0]), stripColor(parts[1]), "", strings.Join(parts[2:], "")
+		}
 	}
+
+	if len(parts) > 2 {
+		// grep - file:line:text
+		lnum := stripColor(parts[1])
+		_, err := strconv.Atoi(lnum)
+		if err == nil {
+			return stripColor(parts[0]), stripColor(parts[1]), "", strings.Join(parts[2:], "")
+		}
+	}
+
+	if len(parts) > 1 {
+		return stripColor(parts[0]), "", "", strings.Join(parts[1:], "")
+	}
+	return stripColor(line), "", "", ""
+}
+
+func formatLine(filepath string, lnum string, cnum string, text string) string {
+	fnameStyle := color.HEX("9da").Sprint
+	dirStyle := color.HEX("888").Sprint
+	sepStyle := color.HEX("666").Sprint
+	lineStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
+	columnStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
+
+	fname := path.Base(filepath)
+	dir := path.Dir(filepath)
+
+	result := fnameStyle(fname)
+	if lnum != "" {
+		result += sepStyle(":") + lineStyle(lnum)
+	}
+	if cnum != "" {
+		result += sepStyle(":") + columnStyle(cnum)
+	}
+	result += " " + dirStyle(dir)
+	if text != "" {
+		result += sepStyle(":") + text
+	}
+
 	return result
+}
+
+func FormatGrep(line string) string {
+	filepath, lnum, cnum, text := splitLine(line)
+	return formatLine(filepath, lnum, cnum, text)
+
+	// fnameStyle := color.HEX("9da").Sprint
+	// lineStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
+	// columnStyle := color.HEX("fed").Sprint //color.New(color.FgHiGreen).SprintFunc()
+	// dimStyle := color.HEX("777").Sprint
+	// parts := strings.SplitN(line, ":", 4)
+	//
+	// filepath := ""
+	// lnum := ""
+	// column := ""
+	// text := ""
+	//
+	// if len(parts) > 0 {
+	// 	filepath = stripColor(parts[0])
+	// }
+	// if len(parts) > 1 {
+	// 	lnum = stripColor(parts[1])
+	// }
+	// if len(parts) > 2 {
+	// 	column = stripColor(parts[2])
+	// }
+	// if len(parts) > 3 {
+	// 	text = parts[3]
+	// }
+	// return formatPrefix(filepath, lnum, column) + text
 
 	// text, err := ansi_parser.Parse(line, ansi_parser.WithIgnoreInvalidCodes(), ansi_parser.ParseOption{NonColorCodes: ansi_parser.Remove})
 	// result := ""
